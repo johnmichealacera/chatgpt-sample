7fc3cb6bdeee9d88599de56ac19bc58b8fc93c18
John Acera <john.acera@sportality.se>
2023-02-02 22:39:14 +0800

    first commit with raw ui for chatgpt testing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="center">
+    <input type="text" name="" id="" v-model="question" class="input-question">
+    <input type="button" value="JM Button Inquiry" @click="loadOpenAi" class="button-input">
+    <div v-if="isLoading" class="loading"></div>
+    <textarea v-if="!isLoading" name="" id="" cols="80" rows="30" v-model="textareaData" disabled></textarea>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from 'vue';
+const { Configuration, OpenAIApi } = require("openai");
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  setup() {
+    const question = ref('');
+    const textareaData = ref('');
+    const isLoading = ref(false);
+    const configuration = new Configuration({
+      apiKey: process.env.VUE_APP_CHATGPT_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const loadOpenAi = async () => {
+      if (question?.value) {
+        isLoading.value = true;
+        const response = await openai.createCompletion({
+          model: "text-davinci-003",
+          prompt: question?.value,
+          temperature: 0.7,
+          max_tokens: 256,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+        });
+        isLoading.value = false;
+        textareaData.value = response?.data?.choices?.[0]?.text;
+      } else {
+        alert('Please enter your inquiry in the text field!');
+      }
+    }
+  
+
+    return {
+      textareaData,
+      question,
+      loadOpenAi,
+      isLoading,
+    }
   }
 }
 </script>
@@ -21,6 +59,24 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.center {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.loading {
+  background: transparent url('https://miro.medium.com/max/882/1*9EBHIOzhE1XfMYoKz1JcsQ.gif') center no-repeat;
+  height: 400px;
+  width: 400px;
+}
+.input-question {
+  padding: 15px;
+  width: 220px;
+}
+.button-input {
+  width: 220px;
 }
 </style>
